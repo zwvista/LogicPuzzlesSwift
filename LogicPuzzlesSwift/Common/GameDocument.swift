082102ecc683a325b858365966a2a5b505f25682
趙偉 <zwvista@msn.com>
2016-12-18 00:03:56 +0900

    moveIndex bug

--- LogicPuzzlesSwift/Common/GameDocument.swift
+++ LogicPuzzlesSwift/Common/GameDocument.swift
@@ -61,12 +61,16 @@ class GameDocument<G: GameBase, GM> {
     
     func moveAdded(game: AnyObject, move: GM) {
         let game = game as! G
-        GameProgress.query().where(withFormat: "gameID = %@ AND levelID = %@ AND moveIndex >= %@", withParameters: [G.gameID, selectedLevelID, game.moveIndex]).fetch().removeAll()
-        
+        let n = game.moveIndex
+        // I don't know why this doesn't work
+        GameProgress.query().where(withFormat: "gameID = %@ AND levelID = %@ AND moveIndex >= %@", withParameters: [G.gameID, selectedLevelID, n]).fetch().removeAll()
+        for case let rec as MoveProgress in moveProgress {
+            if rec.moveIndex >= n {rec.remove()}
+        }
         let rec = MoveProgress()
         rec.gameID = G.gameID
         rec.levelID = selectedLevelID
-        rec.moveIndex = game.moveIndex
+        rec.moveIndex = n
         saveMove(move, to: rec)
         rec.commit()
     }
