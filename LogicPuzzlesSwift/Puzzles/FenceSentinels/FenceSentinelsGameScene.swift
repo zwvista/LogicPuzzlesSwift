ca70603e4d94f8cd881cc1b54ba24a1357bbde5a
趙偉 <zwvista@msn.com>
2017-04-01 15:21:37 +0900

    FenceSentinels 1

---
+++ LogicPuzzlesSwift/Puzzles/FenceSentinels/FenceSentinelsGameScene.swift
@@ -0,0 +1,73 @@
+//
+//  FenceSentinelsGameScene.swift
+//  LogicPuzzlesSwift
+//
+//  Created by 趙偉 on 2016/09/09.
+//  Copyright © 2016年 趙偉. All rights reserved.
+//
+
+import SpriteKit
+
+class FenceSentinelsGameScene: GameScene<FenceSentinelsGameState> {
+    var gridNode: FenceSentinelsGridNode {
+        get {return getGridNode() as! FenceSentinelsGridNode}
+        set {setGridNode(gridNode: newValue)}
+    }
+    
+    func addHint(n: Int, s: HintState, point: CGPoint, nodeName: String) {
+        addLabel(text: String(n), fontColor: s == .normal ? .white : s == .complete ? .green : .red, point: point, nodeName: nodeName)
+    }
+    
+    override func levelInitialized(_ game: AnyObject, state: FenceSentinelsGameState, skView: SKView) {
+        let game = game as! FenceSentinelsGame
+        removeAllChildren()
+        let blockSize = CGFloat(skView.bounds.size.width) / CGFloat(game.cols - 1)
+        
+        // addGrid
+        let offset:CGFloat = 0.5
+        addGrid(gridNode: FenceSentinelsGridNode(blockSize: blockSize, rows: game.rows - 1, cols: game.cols - 1), point: CGPoint(x: skView.frame.midX - blockSize * CGFloat(game.cols - 1) / 2 - offset, y: skView.frame.midY + blockSize * CGFloat(game.rows - 1) / 2 + offset))
+        
+        // addHints
+        for (p, n) in game.pos2hint {
+            let point = gridNode.gridPosition(p: p)
+            let nodeNameSuffix = "-\(p.row)-\(p.col)"
+            let hintNodeName = "hint" + nodeNameSuffix
+            addHint(n: n, s: state.pos2state[p]!, point: point, nodeName: hintNodeName)
+        }
+    }
+    
+    override func levelUpdated(from stateFrom: FenceSentinelsGameState, to stateTo: FenceSentinelsGameState) {
+        for r in 0..<stateFrom.rows {
+            for c in 0..<stateFrom.cols {
+                let p = Position(r, c)
+                let point = gridNode.gridPosition(p: p)
+                let nodeNameSuffix = "-\(r)-\(c)"
+                let horzLineNodeName = "horzLine" + nodeNameSuffix
+                let vertlineNodeName = "vertline" + nodeNameSuffix
+                let hintNodeName = "hint" + nodeNameSuffix
+                func removeHint() { removeNode(withName: hintNodeName) }
+                func removeHorzLine(objType: GridLineObject) {
+                    if objType != .empty { removeNode(withName: horzLineNodeName) }
+                }
+                func removeVertLine(objType: GridLineObject) {
+                    if objType != .empty { removeNode(withName: vertlineNodeName) }
+                }
+                var (o1, o2) = (stateFrom[p][1], stateTo[p][1])
+                if o1 != o2 {
+                    removeHorzLine(objType: o1)
+                    addHorzLine(objType: o2, color: .yellow, point: point, nodeName: horzLineNodeName)
+                }
+                (o1, o2) = (stateFrom[p][2], stateTo[p][2])
+                if o1 != o2 {
+                    removeVertLine(objType: o1)
+                    addVertLine(objType: o2, color: .yellow, point: point, nodeName: vertlineNodeName)
+                }
+                guard let s1 = stateFrom.pos2state[p], let s2 = stateTo.pos2state[p] else {continue}
+                if s1 != s2 {
+                    removeHint()
+                    addHint(n: stateFrom.game.pos2hint[p]!, s: s2, point: point, nodeName: hintNodeName)
+                }
+            }
+        }
+    }
+}
---
+++ LogicPuzzlesSwift/Puzzles/FenceSentinels/FenceSentinelsGameState.swift
@@ -0,0 +1,133 @@
+//
+//  FenceSentinelsGameState.swift
+//  LogicPuzzlesSwift
+//
+//  Created by 趙偉 on 2016/09/19.
+//  Copyright © 2016年 趙偉. All rights reserved.
+//
+
+import Foundation
+
+class FenceSentinelsGameState: GridGameState, FenceSentinelsMixin {
+    // http://stackoverflow.com/questions/24094158/overriding-superclass-property-with-different-type-in-swift
+    var game: FenceSentinelsGame {
+        get {return getGame() as! FenceSentinelsGame}
+        set {setGame(game: newValue)}
+    }
+    var objArray = [GridDotObject]()
+    var pos2state = [Position: HintState]()
+    
+    override func copy() -> FenceSentinelsGameState {
+        let v = FenceSentinelsGameState(game: game)
+        return setup(v: v)
+    }
+    func setup(v: FenceSentinelsGameState) -> FenceSentinelsGameState {
+        _ = super.setup(v: v)
+        v.objArray = objArray
+        v.pos2state = pos2state
+        return v
+    }
+    
+    required init(game: FenceSentinelsGame) {
+        super.init(game: game)
+        objArray = Array<GridDotObject>(repeating: Array<GridLineObject>(repeating: .empty, count: 4), count: rows * cols)
+        for p in game.pos2hint.keys {
+            pos2state[p] = .normal
+        }
+        updateIsSolved()
+    }
+    
+    subscript(p: Position) -> GridDotObject {
+        get {
+            return self[p.row, p.col]
+        }
+        set(newValue) {
+            self[p.row, p.col] = newValue
+        }
+    }
+    subscript(row: Int, col: Int) -> GridDotObject {
+        get {
+            return objArray[row * cols + col]
+        }
+        set(newValue) {
+            objArray[row * cols + col] = newValue
+        }
+    }
+    
+    func setObject(move: inout FenceSentinelsGameMove) -> Bool {
+        var changed = false
+        func f(o1: inout GridLineObject, o2: inout GridLineObject) {
+            if o1 != move.obj {
+                changed = true
+                o1 = move.obj
+                o2 = move.obj
+                // updateIsSolved() cannot be called here
+                // self[p] will not be updated until the function returns
+            }
+        }
+        let p = move.p
+        let dir = move.dir, dir2 = (dir + 2) % 4
+        f(o1: &self[p][dir], o2: &self[p + FenceSentinelsGame.offset[dir]][dir2])
+        if changed {updateIsSolved()}
+        return changed
+    }
+    
+    func switchObject(move: inout FenceSentinelsGameMove) -> Bool {
+        let markerOption = MarkerOptions(rawValue: self.markerOption)
+        func f(o: GridLineObject) -> GridLineObject {
+            switch o {
+            case .empty:
+                return markerOption == .markerFirst ? .marker : .line
+            case .line:
+                return markerOption == .markerLast ? .marker : .empty
+            case .marker:
+                return markerOption == .markerFirst ? .line : .empty
+            }
+        }
+        let o = f(o: self[move.p][move.dir])
+        move.obj = o
+        return setObject(move: &move)
+    }
+    
+    private func updateIsSolved() {
+        isSolved = true
+        for (p, n2) in game.pos2hint {
+            var n1 = 0
+            for i in 0..<4 {
+                if self[p + FenceSentinelsGame.offset2[i]][FenceSentinelsGame.dirs[i]] != .line {n1 += 1}
+            }
+            pos2state[p] = n1 < n2 ? .normal : n1 == n2 ? .complete : .error
+            if n1 != n2 {isSolved = false}
+        }
+        guard isSolved else {return}
+        let g = Graph()
+        var pos2node = [Position: Node]()
+        for r in 0..<rows {
+            for c in 0..<cols {
+                let p = Position(r, c)
+                let n = self[p].filter({$0 == .line}).count
+                switch n {
+                case 0:
+                    continue
+                case 2:
+                    pos2node[p] = g.addNode(p.description)
+                default:
+                    isSolved = false
+                    return
+                }
+            }
+        }
+        for p in pos2node.keys {
+            let dotObj = self[p]
+            for i in 0..<4 {
+                guard dotObj[i] == .line else {continue}
+                let p2 = p + FenceSentinelsGame.offset[i]
+                g.addEdge(pos2node[p]!, neighbor: pos2node[p2]!)
+            }
+        }
+        let nodesExplored = breadthFirstSearch(g, source: pos2node.values.first!)
+        let n1 = nodesExplored.count
+        let n2 = pos2node.values.count
+        if n1 != n2 {isSolved = false}
+    }
+}
