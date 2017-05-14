ca61eb38bd6a481807576679ab9eeadc47cd6346
zwvista <zwvista@msn.com>
2017-05-14 21:57:47 +0900

    Help 6

---
+++ LogicPuzzlesSwift/Puzzles/BoxItAround/BoxItAroundHelpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BoxItAroundHelpViewController.swift
+//  LogicPuzzlesSwift
+//
+//  Created by 趙偉 on 2017/05/14.
+//  Copyright © 2017年 趙偉. All rights reserved.
+//
+
+import UIKit
+
+class BoxItAroundHelpViewController: GameHelpViewController, BoxItAroundMixin {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameDocument.help.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath) as! SelfSizingTableViewCell
+        cell.label.text = gameDocument.help[indexPath.row]
+        return cell;
+    }
+}
---
+++ LogicPuzzlesSwift/Puzzles/Orchards/OrchardsHelpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OrchardsHelpViewController.swift
+//  LogicPuzzlesSwift
+//
+//  Created by 趙偉 on 2017/05/14.
+//  Copyright © 2017年 趙偉. All rights reserved.
+//
+
+import UIKit
+
+class OrchardsHelpViewController: GameHelpViewController, OrchardsMixin {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameDocument.help.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath) as! SelfSizingTableViewCell
+        cell.label.text = gameDocument.help[indexPath.row]
+        return cell;
+    }
+}
