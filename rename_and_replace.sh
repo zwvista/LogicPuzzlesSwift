dd1f2420cf31adcda82d88c20c11b3f135e7394b
趙偉 <zwvista@msn.com>
2025-10-16 10:58:34 +0900

    NumberCrossing 1

--- rename_and_replace.sh
+++ rename_and_replace.sh
@@ -2,11 +2,11 @@
 set -e
 
 # 协程变量（源字符串和目标字符串）
-SRC="FourMeNot"
-DST="TurnTwice"
+SRC="Sumscrapers"
+DST="NumberCrossing"
 
 # 切换到 Branches 目录
-cd "$(dirname "$0")/LogicPuzzlesSwift/Puzzles/TurnTwice"
+cd "$(dirname "$0")/LogicPuzzlesSwift/Puzzles/NumberCrossing"
 
 # 1. 改文件名
 for f in ${SRC}*; do
