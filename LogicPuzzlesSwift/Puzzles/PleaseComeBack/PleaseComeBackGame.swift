d2aaafa4e77bc4d0bf04ebf86d0c7affc1917f32
趙偉 <zwvista@msn.com>
2026-01-21 13:27:21 +0900

    PleaseComeBack 2

--- LogicPuzzlesSwift/Puzzles/PleaseComeBack/PleaseComeBackGame.swift
+++ LogicPuzzlesSwift/Puzzles/PleaseComeBack/PleaseComeBackGame.swift
@@ -11,8 +11,81 @@ import Foundation
 class PleaseComeBackGame: GridGame<PleaseComeBackGameState> {
     static let PUZ_BLOCK: Character = "B"
     static let offset = Position.Directions4
-
+    static let offset2 = [
+        Position(0, 0),
+        Position(1, 1),
+        Position(1, 1),
+        Position(0, 0),
+    ]
+    static let dirs = [1, 0, 3, 2]
+    
+    var areas = [[Position]]()
+    var pos2area = [Position: Int]()
+    var dots: GridDots!
     var objArray = [Character]()
+    
+    init(layout: [String], delegate: PleaseComeBackGameViewController? = nil) {
+        super.init(delegate: delegate)
+        
+        size = Position(layout.count / 2, layout[0].length / 2)
+        dots = GridDots(rows: rows + 1, cols: cols + 1)
+        objArray = Array<Character>(repeating: " ", count: rows * cols)
+
+        for r in 0..<rows + 1 {
+            var str = layout[2 * r]
+            for c in 0..<cols {
+                let ch = str[2 * c + 1]
+                if ch == "-" {
+                    dots[r, c][1] = .line
+                    dots[r, c + 1][3] = .line
+                }
+            }
+            guard r < rows else {break}
+            str = layout[2 * r + 1]
+            for c in 0..<cols + 1 {
+                let ch = str[2 * c]
+                if ch == "|" {
+                    dots[r, c][2] = .line
+                    dots[r + 1, c][0] = .line
+                }
+                guard c < cols else {continue}
+                let ch2 = str[2 * c + 1]
+                self[r, c] = ch2
+            }
+        }
+        let g = Graph()
+        var pos2node = [Position: Node]()
+        for r in 0..<rows {
+            for c in 0..<cols {
+                let p = Position(r, c)
+                pos2node[p] = g.addNode(p.description)
+            }
+        }
+        for r in 0..<rows {
+            for c in 0..<cols {
+                let p = Position(r, c)
+                for i in 0..<4 {
+                    if dots[p + RippleEffectGame.offset2[i]][RippleEffectGame.dirs[i]] != .line {
+                        g.addEdge(pos2node[p]!, neighbor: pos2node[p + RippleEffectGame.offset[i]]!)
+                    }
+                }
+            }
+        }
+        while !pos2node.isEmpty {
+            let nodesExplored = breadthFirstSearch(g, source: pos2node.first!.value)
+            let area = pos2node.filter { nodesExplored.contains($0.1.label) }.map { $0.0 }
+            pos2node = pos2node.filter { !nodesExplored.contains($0.1.label) }
+            let n = areas.count
+            for p in area {
+                pos2area[p] = n
+            }
+            areas.append(area)
+        }
+        
+        let state = PleaseComeBackGameState(game: self)
+        levelInitialized(state: state)
+    }
+
     subscript(p: Position) -> Character {
         get { self[p.row, p.col] }
         set { self[p.row, p.col] = newValue }
@@ -22,19 +95,4 @@ class PleaseComeBackGame: GridGame<PleaseComeBackGameState> {
         set { objArray[row * cols + col] = newValue }
     }
     
-    init(layout: [String], delegate: PleaseComeBackGameViewController? = nil) {
-        super.init(delegate: delegate)
-        
-        size = Position(layout.count, layout[0].length)
-        objArray = Array<Character>(repeating: " ", count: rows * cols)
-        
-        for r in 0..<rows {
-            let str = layout[r]
-            for c in 0..<cols {
-                self[r, c] = str[c]
-            }
-        }
-        let state = PleaseComeBackGameState(game: self)
-        levelInitialized(state: state)
-    }
 }
--- LogicPuzzlesSwift/Puzzles/PleaseComeBack/PleaseComeBackGameScene.swift
+++ LogicPuzzlesSwift/Puzzles/PleaseComeBack/PleaseComeBackGameScene.swift
@@ -23,6 +23,33 @@ class PleaseComeBackGameScene: GameScene<PleaseComeBackGameState> {
         let offset:CGFloat = 0.5
         addGrid(gridNode: PleaseComeBackGridNode(blockSize: blockSize, rows: game.rows, cols: game.cols), point: CGPoint(x: skView.frame.midX - blockSize * CGFloat(game.cols) / 2 - offset, y: skView.frame.midY + blockSize * CGFloat(game.rows) / 2 + offset))
         
+        let pathToDraw = CGMutablePath()
+        let lineNode = SKShapeNode(path: pathToDraw)
+        for r in 0..<game.rows + 1 {
+            for c in 0..<game.cols + 1 {
+                let p = Position(r, c)
+                let point = gridNode.centerPoint(p: p)
+                for dir in 1...2 {
+                    guard game.dots[r, c][dir] == .line else {continue}
+                    switch dir {
+                    case 1:
+                        pathToDraw.move(to: CGPoint(x: point.x - gridNode.blockSize / 2, y: point.y + gridNode.blockSize / 2))
+                        pathToDraw.addLine(to: CGPoint(x: point.x + gridNode.blockSize / 2, y: point.y + gridNode.blockSize / 2))
+                        lineNode.glowWidth = 8
+                    case 2:
+                        pathToDraw.move(to: CGPoint(x: point.x - gridNode.blockSize / 2, y: point.y + gridNode.blockSize / 2))
+                        pathToDraw.addLine(to: CGPoint(x: point.x - gridNode.blockSize / 2, y: point.y - gridNode.blockSize / 2))
+                        lineNode.glowWidth = 8
+                    default:
+                        break
+                    }
+                }
+            }
+        }
+        lineNode.path = pathToDraw
+        lineNode.name = "line"
+        gridNode.addChild(lineNode)
+
         // add Numbers
         for r in 0..<game.rows {
             for c in 0..<game.cols {
